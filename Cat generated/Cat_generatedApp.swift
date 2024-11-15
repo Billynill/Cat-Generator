5eb9a377cfef5e9c0b4065da3b04bebaa082fa0f
Billynill <avezzzov_r@mail.ru>
2024-11-15 11:27:09 +0300

    first one

---
+++ Cat generated/Cat_generatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_generatedApp.swift
+//  Cat generated
+//
+//  Created by 596 on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Cat_generatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat generated/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cat generated
+//
+//  Created by 596 on 14.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
